c89094e3c0bb2e30790e43a00273300f683e2d78
Dustin Pearce <dpearce@unknown002374e326e3.gateway.2wire.net>
2013-02-07 10:07:09 -0800

    search results

--- scripts/junior/answers-global.js
+++ scripts/junior/answers-global.js
@@ -1,3 +1,7 @@
 $("#search-toggle").bind("touchstart",function(){
     $("#search").toggle();
 });
+
+$("#search-button").bind("touchstart",function(){
+	location.href = "index-search-result.html";
+});
